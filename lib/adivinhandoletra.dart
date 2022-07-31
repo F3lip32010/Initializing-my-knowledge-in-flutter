575fa9d7ae8ccc47f14aee52e7e1a7346a646ab1
Felipe <felipemeirelessantana@gmail.com>
2022-07-31 14:21:33 -0300

    c3 - validator

--- lib/adivinhandoletra.dart
+++ lib/adivinhandoletra.dart
@@ -8,6 +8,7 @@ class adivinhandoLetra extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     final TextEditingController nameController = TextEditingController();
     final TextEditingController letterController = TextEditingController();
 
@@ -15,125 +16,134 @@ class adivinhandoLetra extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Guessing the Lyrics'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              controller: nameController,
-              onChanged: (value) {
-                //nameController.text = value;
-                log(nameController.text);
-              },
-              validator: (value) {
-                if (value != null) {
-                  if (value.isEmpty) {
-                    return "Necessário digitar o nome";
-                  }
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                hintText: "Type in your text",
-                labelText: 'Name',
-                fillColor: Colors.white70,
-                prefixIcon: const Icon(Icons.person),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              controller: letterController,
-              onChanged: (value) {
-                //letterController.text = value;
-                log(letterController.text);
-              },
-              decoration: InputDecoration(
+              TextFormField(
+                controller: nameController,
+                onChanged: (value) {
+                  //nameController.text = value;
+                  log(nameController.text);
+                },
+                validator: (value) {
+                  if (value!.length < 3) {
+                    return "Necessário digitar no mínimo 3 caracteres";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(100),
                   ),
                   filled: true,
                   hintStyle: TextStyle(color: Colors.grey[800]),
                   hintText: "Type in your text",
-                  labelText: 'Letter',
+                  labelText: 'Name',
                   fillColor: Colors.white70,
-                  prefixIcon: const Icon(Icons.abc_rounded)),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
+                  prefixIcon: const Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: letterController,
+                onChanged: (value) {
+                  //letterController.text = value;
+                  log(letterController.text);
+                },
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Need to type the letter";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100),
                     ),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    hintText: "Type in your text",
+                    labelText: 'Letter',
+                    fillColor: Colors.white70,
+                    prefixIcon: const Icon(Icons.abc_rounded)),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (_) => AlertDialog(
-                            actions: [
-                              IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Icons.abc_outlined))
-                            ],
-                            title: const Text('Olá Brasil'),
-                          ));
-                  String name = nameController.text.toUpperCase();
-                  String letter = letterController.text.toUpperCase();
-                  bool resposta = name.contains(letter);
-                  log('$resposta');
-                  if (resposta) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        backgroundColor: Colors.green,
-                        content: const Text('True Letter!'),
-                        action: SnackBarAction(
-                          label: 'Action',
-                          onPressed: () {
-                            // Code to execute.
-                          },
-                        ),
-                      ),
-                    );
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        backgroundColor: Colors.red,
-                        action: SnackBarAction(
-                          label: 'Action',
-                          onPressed: () {
-                            // Code to execute.
-                          },
-                        ),
-                        content: const Text('Awesome SnackBar!'),
-                        duration: const Duration(milliseconds: 1500),
-                        width: 280.0, // Width of the SnackBar.
-                        padding: const EdgeInsets.symmetric(
-                          horizontal:
-                              8.0, // Inner padding for SnackBar content.
-                        ),
-                        behavior: SnackBarBehavior.floating,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Verify Letter')),
-          ],
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      showDialog(
+                          context: context,
+                          builder: (_) => AlertDialog(
+                                actions: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.abc_outlined))
+                                ],
+                                title: const Text('Olá Brasil'),
+                              ));
+                      String name = nameController.text.toUpperCase();
+                      String letter = letterController.text.toUpperCase();
+                      bool resposta = name.contains(letter);
+                      log('$resposta');
+                      if (resposta) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.green,
+                            content: const Text('True Letter!'),
+                            action: SnackBarAction(
+                              label: 'Action',
+                              onPressed: () {
+                                // Code to execute.
+                              },
+                            ),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.red,
+                            action: SnackBarAction(
+                              label: 'Action',
+                              onPressed: () {
+                                // Code to execute.
+                              },
+                            ),
+                            content: const Text('Awesome SnackBar!'),
+                            duration: const Duration(milliseconds: 1500),
+                            width: 280.0, // Width of the SnackBar.
+                            padding: const EdgeInsets.symmetric(
+                              horizontal:
+                                  8.0, // Inner padding for SnackBar content.
+                            ),
+                            behavior: SnackBarBehavior.floating,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Verify Letter')),
+            ],
+          ),
         ),
       ),
     );
